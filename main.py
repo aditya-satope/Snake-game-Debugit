2a49d16a21a18f7e2a4618c56b0473cd79922d63
Aditya Satope <adityasatope@gmail.com>
2022-04-22 19:25:04 +0530

    second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 from Game import game_on
+
 from Screen import *
 import keyboard
 screen = Screen()
@@ -8,11 +9,12 @@ screen.setup(width=900, height=800)
 screen.bgcolor('black')
 screen.title('Snake game!!!!')
 home_screen()
- 
+
 
 screen.listen()
 
-
+print('hi')
+print('foiasjdfopj')
 screen.onkeypress(game_on, 'Return')
 
 
